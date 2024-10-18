12dcae1956ea9f5f236f226d40cecd3269f2c5dc
alan <al6nlee@gmail.com>
2024-10-18 14:42:08 +0800

    日志打印，追加客户端IP地址

--- api/app/initialization/middleware.py
+++ api/app/initialization/middleware.py
@@ -24,8 +24,12 @@ def register_middleware(app):
             user_id = current_user.user_id if current_user else None
             time_cost = int((time.monotonic() - g.start) * 1000)
             length = response.headers.get("Content-Length", type=int)
+            if 'X-Forwarded-For' in request.headers:
+                user_ip = request.headers['X-Forwarded-For'].split(',')[0]  # 获取第一个 IP 地址
+            else:
+                user_ip = request.remote_addr
             logger.info(
-                f'User<{user_id}> {request.method.upper()} "{request.path}" => '
+                f'User<{user_id}> IP<{user_ip}> {request.method.upper()} "{request.path}" => '
                 f"generated {length} bytes in {time_cost} msecs, "
                 f"status_code: {response.status_code}, "
                 f"mimetype: {response.mimetype}"
