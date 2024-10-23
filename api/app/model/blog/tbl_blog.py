14ebe8112a53ae64a750669b8cca1567a98d5c59
alan <al6nlee@gmail.com>
2024-10-23 20:33:00 +0800

    新增摘要字段

--- api/app/model/blog/tbl_blog.py
+++ api/app/model/blog/tbl_blog.py
@@ -6,6 +6,7 @@ class Blog(BaseModel):
     __tablename__ = 'tbl_blog'
 
     title = db.Column(db.String(200), nullable=False, comment='博客标题')
+    abstract = db.Column(db.String(1024), nullable=False, comment='摘要')
     category_id = db.Column(db.Integer, nullable=True, comment='分类ID')
     author_id = db.Column(db.Integer, nullable=False, comment='作者ID')
     is_draft = db.Column(db.Boolean, default=True, comment='是否为草稿')
